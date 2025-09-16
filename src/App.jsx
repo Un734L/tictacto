a15deed5fbb31debe1c3ee38aab418cd81480610
George <manassehmali@gmail.com>
2025-09-16 12:17:27 +0200

    made different rows of the boad using buttons

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,28 @@ import './App.css'
 
 function App() {
   return (
-    <button className='square'>X</button>
-  );
-}
+    <>
+    <div className='board-row'>
 
+      <button className='square'>1</button>
+      <button className='square'>2</button>
+      <button className='square'>3</button>
+    </div>
+
+    // 2nd row
+    <div>
+      <button className='square'>4</button>
+      <button className='square'>5</button>
+      <button className='square'>6</button>
+    </div>
+    
+    // 3rd rom
+    <div>
+    <button className='square'>X</button>
+    <button className='square'>X</button>
+    <button className='square'>X</button>
+    </div>
+    </>
+  )
+}
 export default App
