13d6a4154732f062f0c0a3a2f2e50a989bead5c2
George <manassehmali@gmail.com>
2025-09-16 13:10:03 +0200

    used a usestate hook to manipulate the display

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,46 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+function Square({props, onSquareClick}){
+
+    return <button onClick={onSquareClick} className='square'>{props}</button>
+  }
+  
 function App() {
+  const [xIsNext,setXIsNext]=useState(true);
+  const [sqaures,setSquares]=useState(Array(9).fill(null));
+
+  function handleClick(i){
+    const nextSquares=sqaures.slice();
+    if(xIsNext){
+
+      nextSquares[i]="X";
+    }else{
+      nextSquares[i]="O";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
   return (
     <>
     <div className='board-row'>
+      <Square props={sqaures[0]} onSquareClick={()=>handleClick(0)} />
+      <Square props={sqaures[1]} onSquareClick={()=>handleClick(1)}/>
+      <Square props={sqaures[2]} onSquareClick={()=>handleClick(2)}/>
 
-      <button className='square'>1</button>
-      <button className='square'>2</button>
-      <button className='square'>3</button>
     </div>
 
-    // 2nd row
-    <div>
-      <button className='square'>4</button>
-      <button className='square'>5</button>
-      <button className='square'>6</button>
+    <div className='board-row'>
+      <Square props={sqaures[3]} onSquareClick={()=>handleClick(3)}/>
+      <Square props={sqaures[4]} onSquareClick={()=>handleClick(4)}/>
+      <Square props={sqaures[5]} onSquareClick={()=>handleClick(5)}/> 
     </div>
-    
-    // 3rd rom
-    <div>
-    <button className='square'>X</button>
-    <button className='square'>X</button>
-    <button className='square'>X</button>
+
+    <div className='board-row'>
+      <Square props={sqaures[6]} onSquareClick={()=>handleClick(6)}/>
+      <Square props={sqaures[7]} onSquareClick={()=>handleClick(7)}/>
+      <Square props={sqaures[8]} onSquareClick={()=>handleClick(8)}/>
     </div>
     </>
   )
